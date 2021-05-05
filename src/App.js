b598040d43255f5bea8837ed238196c6b2840119
pratyush624 <pratyush624@gmail.com>
2021-05-05 22:53:50 +0530

    Live Weather

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { getWeatherData } from "./weatherapi";
+
+import { ScaleLoader } from "react-spinners";
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("Bangalore");
+  const [loading, setLoading] = useState(false);
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const data = await getWeatherData(city);
+      setWeatherData(data);
+      // console.log(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error.message);
+      setLoading(false);
+    }
+  };
+
+  const override = `display:block;
+  margin:0 auto;
+  border-color:red;
+  `;
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <h2 className="title">
+          <i className="fa fa-cloud">Weather App</i>
+        </h2>
+        <div className="search-form">
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            placeholder="Enter your city name"
+          />
+          <button type="button" onClick={() => getData()}>
+            Search
+          </button>
+        </div>
+
+        {loading ? (
+          <div className="loader-container">
+            <ScaleLoader
+              css={override}
+              size={200}
+              color={"#fff"}
+              loading={loading}
+            />
+          </div>
+        ) : (
+          <>
+            {weatherData !== null ? (
+              <div className="main-container">
+                <h4>Live Weather Condition</h4>
+                <div className="weather-icon">
+                  <img
+                    src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+                    alt="imgicon"
+                  />
+                </div>
+                <h3>{weatherData.weather[0].main}</h3>
+                <div className="temperature">
+                  <h1>
+                    {parseFloat(weatherData.main.temp - 273.15).toFixed(1)}
+                    &deg;C
+                  </h1>
+                </div>
+                <div className="location">
+                  <h3>
+                    <i className="fa fa-street-view"></i>
+                    {weatherData.name} | {weatherData.sys.country}
+                  </h3>
+                </div>
+                <div className="temperature-range">
+                  <h5>
+                    Min:{" "}
+                    {parseFloat(weatherData.main.temp_min - 273.15).toFixed(1)}
+                    &deg;C || Max:{" "}
+                    {parseFloat(weatherData.main.temp_max - 273.15).toFixed(1)}
+                    &deg;C || Humidity: {weatherData.main.humidity}%{" "}
+                  </h5>
+                </div>
+              </div>
+            ) : null}
+          </>
+        )}
+      </div>
     </div>
   );
 }

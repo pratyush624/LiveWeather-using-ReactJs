c884a739b4868f9134c966ac81303a4ab6cf90f9
Pratyush <53374465+pratyush624@users.noreply.github.com>
2021-05-06 07:22:31 +0530

    Update weatherapi.js

--- src/weatherapi.js
+++ src/weatherapi.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
+var cors_api_host = ' ‘https://cors-anywhere.herokuapp.com/252'
 const baseUrl = "http://api.openweathermap.org/data/2.5/weather?";
 const apiKey = "7ce934af099025072871fce329d264b4";
 
 export const getWeatherData = async (cityname) => {
   try {
-    const {data} = await axios.get(baseUrl + `q=${cityname}&appid=${apiKey}`);
+    const {data} = await axios.get(baseUrl + cors_api_host + `q=${cityname}&appid=${apiKey}`);
     return data
   } catch (error) {
     throw error;
